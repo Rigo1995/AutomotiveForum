e179d64ad79c5a4ce0b7ed8c097380de9f922de4
rigo <rigobertoramirez606@gmail.com>
2018-07-27 13:29:55 -0500

    first commit

---
+++ AutomotiveForum/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AutomotiveForum.MvcApplication" Language="C#" %>
---
+++ AutomotiveForum/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AutomotiveForum.Startup))]
+namespace AutomotiveForum
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
