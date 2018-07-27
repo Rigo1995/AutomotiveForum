48cbac7542a53d9f08f6721622c6d879f4251b59
rigo <rigobertoramirez606@gmail.com>
2018-07-27 14:30:50 -0500

    plugged in built in roles from mvc will be creating the views for the forum next

--- AutomotiveForum/Startup.cs
+++ AutomotiveForum/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using AutomotiveForum.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(AutomotiveForum.Startup))]
@@ -9,6 +12,26 @@ namespace AutomotiveForum
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateRoles();
         }
+
+        private void CreateRoles()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<Microsoft.AspNet.Identity.EntityFramework.IdentityRole>(context));
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+            }
+            if (!roleManager.RoleExists("Customer"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Customer";
+                roleManager.Create(role);
+            }
+        }
+
     }
 }
