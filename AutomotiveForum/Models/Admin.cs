8578690aefd66c117dd6e627c352075966df8e72
rigo <rigobertoramirez606@gmail.com>
2018-08-02 07:40:25 -0500

    added marketplace mvc

---
+++ AutomotiveForum/Models/Admin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AutomotiveForum.Models
+{
+    public class Admin
+    {
+        [Key]
+        public int ID { get; set; }
+        [Display(Name = "Admin Name")]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public List<Customers> customers { get; set; }
+        public List<Comments> comments { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationUserID { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
