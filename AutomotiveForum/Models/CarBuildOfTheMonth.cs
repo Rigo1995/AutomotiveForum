8bb684d1f95f1e5faed696aff6fde410ef723e55
rigo <rigobertoramirez606@gmail.com>
2018-08-02 14:17:04 -0500

    forum,marketplace are working just need to add the carbuild of the month along with the ability to comment on the forum page

---
+++ AutomotiveForum/Models/CarBuildOfTheMonth.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AutomotiveForum.Models
+{
+    public class CarBuildOfTheMonth
+    {
+        [Key]
+        public int Name { get; set; }
+        public DateTime? Date { get; set; }
+        public string Entry { get; set; }
+
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+    }
+}
